824e581884f399b0f3e588692e2795bc0b183fe8
Carl <nope@no.nah>
2016-10-11 10:09:15 +0000

    Add barebones framework.

---
+++ main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import pygame
+import time
+from pygame.locals import *
+
+# picture display size
+width = 1280
+height = 800
+
+windowSurfaceObj = pygame.display.set_mode((width,height))
+pygame.display.set_caption('picture')
+
+image_queue = list()
+
+# Do producer-consumer pattern:
+# producer process ensures image queue constantly has 150 images indefinitely,
+# keeping track of the last loaded image, wrapping around when last frame of video loaded
+# consumer process takes frames off the queue at a variable rate, but no faster than
+# half of the frames in the queue per second - may need to tweak max queue size, max speed
+
+pygame.display.toggle_fullscreen()
+
+# early producer code
+i = 1
+while i <= 150:
+  filename = "frame" + "%03d" % (i,) + ".png"
+  image_queue.append(pygame.image.load(filename))
+  i += 1
+
+# early consumer code
+while image_queue:
+  time.sleep(20.0 / 1000.0)
+  windowSurfaceObj.blit(image_queue.pop(0),(0,0))
+  pygame.display.update()
+  
