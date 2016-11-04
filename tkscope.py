f8d59554cbf593f819df9c41156a0336c7d93f6f
Carl <nope@no.nah>
2016-11-04 22:00:55 +0000

    Implemented rate control into tkscope

--- tkscope.py
+++ tkscope.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 
+import ctypes
 import vlc
 import sys
+import smoother
+import triggers
 from Tkinter import Tk, Frame, BOTH
 
 class TkScope(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent, background="black")   
          
+        self.paused = False
         self.parent = parent
+        self.parent.attributes("-fullscreen", True)
         self.instance = vlc.Instance()
         self.player = self.instance.media_player_new()
         self.event_manager = self.player.event_manager()
@@ -24,8 +29,17 @@ class TkScope(Frame):
         self.player.play()
 
     def set_rate(self, rate):
-        self.player.set_rate(rate)
-        self.parent.after(5000, lambda: self.set_rate(1.5))
+        if rate <= 0.5:
+            if not self.paused:
+                self.player.pause()
+                self.paused = True
+                print "Paused..."
+        else:
+            if self.paused:
+                self.player.play()
+                self.paused = False
+                print "Unpaused..."
+            self.player.set_rate(rate)
 
     @vlc.callbackmethod
     def loop_it(self, event, player):
@@ -36,12 +50,39 @@ class TkScope(Frame):
         self.player.set_media(self.media)
         self.player.set_xwindow(self.winfo_id())
         self.player.play()
+        self.set_rate(0.0)
         
+def start_smoother(root, period, smoother):
+    root.after(period, lambda: update_smoother(root, period, smoother))
+
+def update_smoother(root, period, smoother):
+    smoother.update()
+    start_smoother(root, period, smoother)
+
+def start_rate(root, period, player, smoother):
+    root.after(period, lambda: update_rate(root, period, player, smoother))
+
+def update_rate(root, period, player, smoother):
+    x = smoother.sample() * 1.5 + 0.5
+    print("rate: {}".format(x))
+    player.set_rate(x)
+    start_rate(root, period, player, smoother)
+
 if __name__ == '__main__':
+    x11 = ctypes.cdll.LoadLibrary('libX11.so')
+    x11.XInitThreads()
+
     root = Tk()
+    root.geometry('{}x{}'.format(1280, 800))
+    root.config(cursor='none')
+
+    trigger = triggers.RandomSineTrigger(5)
+    smoother = smoother.Smoother(trigger, 10)
 
     player = TkScope(root)
     player.open(sys.argv[1])
-    root.after(5000, lambda: player.set_rate(0.1))
+
+    start_smoother(root, 50, smoother)
+    start_rate(root, 200, player, smoother)
 
     root.mainloop()
